91f32189d9999dcc220377a8fd4b332ee7f42de7
Monark Jain <jainmonark145@gmail.com>
2022-02-01 16:23:30 +0530

    created carousels and navbar

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
-  
+import React, { Component } from "react";
+import Nav from "react-bootstrap/Nav";
+
 class About extends Component {
   render() {
     return (
       <div>
         <h1>About</h1>
+        <Nav variant="tabs" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav variant="pills" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
-    )
+    );
   }
 }
 
-export default About
+export default About;
--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react'
-  
-const Home=()=> {
-   
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <ControlledCarousel></ControlledCarousel>
+    </div>
+  );
+};
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://wallpapercave.com/wp/wp6921036.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i0.wp.com/www.toppapeldeparede.com.br/wp-content/uploads/2021/04/50-Minimalist-Desktop-Wallpapers-and.png?fit=1200%2C675&ssl=1"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://assets.hongkiat.com/uploads/beautiful-minimalist-desktop-wallpapers/4k/original/01.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Home
+export default Home;
